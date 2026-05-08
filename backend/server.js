92e83aa04475613371cdf3f1a195800a33cbe30b
mustapha-jaiteh <neymarjaiteh@yahoo.com>
2026-05-08 14:55:00 +0000

    server-port update 09/05/26

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ app.use("/uploads", express.static(path.join(path.resolve(), "uploads")));
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
